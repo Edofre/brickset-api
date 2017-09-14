1dde7c4fe62fab6d5162a0140a23b3d19e612725
Edo Freriks <edofre@gmail.com>
2017-09-14 21:33:57 +0200

    Add dumping first set

--- src/index.php
+++ src/index.php
@@ -1,7 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: edofreriks
- * Date: 12/09/2017
- * Time: 22:12
- */
+$names = file('../data/sets.txt');
+
+foreach ($names as &$name) {
+	$name = trim($name) . '-1';
+}
+
+$url = 'https://brickset.com/api/v2.asmx/getSets?';
+$query = http_build_query([
+	'apiKey'     => 'XXX',
+	'userHash'   => 'XXX',
+	'setNumber'  => 'XXX',
+	'query'      => '',
+	'theme'      => '',
+	'subtheme'   => '',
+	'year'       => '',
+	'owned'      => '',
+	'wanted'     => '',
+	'pageNumber' => '',
+	'pageSize'   => '',
+	'orderBy'    => '',
+	'userName'   => '',
+]);
+
+var_dump($query);
+var_dump($url);
+
+$curl = curl_init();
+curl_setopt_array($curl, [
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_URL            => $url . $query,
+]);
+$resp = curl_exec($curl);
+curl_close($curl);
+
+
+$xml = simplexml_load_string($resp);
+if ($xml === false) {
+	echo "Failed loading XML: ";
+	foreach (libxml_get_errors() as $error) {
+		echo "<br>", $error->message;
+	}
+} else {
+	var_dump($xml);
+}
+?>
