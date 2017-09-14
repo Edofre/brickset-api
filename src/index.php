fc0c4d14ea08baa955481d6b59114e576947e8fc
Edo Freriks <edofre@gmail.com>
2017-09-14 22:08:01 +0200

    Loop sets + write to file

--- src/index.php
+++ src/index.php
@@ -1,15 +1,17 @@
 <?php
-$names = file('../data/sets.txt');
 
-foreach ($names as &$name) {
-	$name = trim($name) . '-1';
-}
+// Load data
+$sets = file('../data/sets.txt');
+
+// API auth details
+$apiKey = 'x';
+$userHash = 'x';
 
 $url = 'https://brickset.com/api/v2.asmx/getSets?';
-$query = http_build_query([
-	'apiKey'     => 'XXX',
-	'userHash'   => 'XXX',
-	'setNumber'  => 'XXX',
+$queryDetails = [
+	'apiKey'     => $apiKey,
+	'userHash'   => $userHash,
+	'setNumber'  => '',
 	'query'      => '',
 	'theme'      => '',
 	'subtheme'   => '',
@@ -20,27 +22,42 @@ $query = http_build_query([
 	'pageSize'   => '',
 	'orderBy'    => '',
 	'userName'   => '',
-]);
+];
 
-var_dump($query);
-var_dump($url);
+// Open te file we'll write the prices to
+$pricesFile = fopen("../output/setPrices.txt", "w") or die("Unable to open file!");
 
 $curl = curl_init();
-curl_setopt_array($curl, [
-	CURLOPT_RETURNTRANSFER => 1,
-	CURLOPT_URL            => $url . $query,
-]);
-$resp = curl_exec($curl);
+foreach ($sets as &$set) {
+	// Add a -1 to the setname because we need to specify the version
+	$set = trim($set) . '-1';
+
+	$queryDetails['setNumber'] = $set;
+	$query = http_build_query($queryDetails);
+	curl_setopt_array($curl, [
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL            => $url . $query,
+	]);
+	$resp = curl_exec($curl);
+	$xml = simplexml_load_string($resp);
+	if ($xml === false) {
+		echo "Failed loading XML: ";
+		foreach (libxml_get_errors() as $error) {
+			echo "<br>", $error->message;
+		}
+	} else {
+		// Fetch variables we need to write
+		$setName = (string)$xml->sets->name;
+		$setNumber = (string)$xml->sets->number;
+		$setPrice = (float)$xml->sets->EURetailPrice;
+
+		// And write them to the file
+		fwrite($pricesFile, "$setNumber \t $setPrice \t $setName \n");
+	}
+}
 curl_close($curl);
 
+fclose($pricesFile);
 
-$xml = simplexml_load_string($resp);
-if ($xml === false) {
-	echo "Failed loading XML: ";
-	foreach (libxml_get_errors() as $error) {
-		echo "<br>", $error->message;
-	}
-} else {
-	var_dump($xml);
-}
+var_dump("Done writing to /output/setPrices.txt");
 ?>
